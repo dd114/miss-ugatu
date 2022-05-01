62f8b1ce2d138ecd82758c90447d0583e616cdda
dd114 <40179423+dd114@users.noreply.github.com>
2022-05-02 01:39:17 +0500

    improved game has been added

--- src/App.js
+++ src/App.js
@@ -1,115 +1,152 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import bridge from '@vkontakte/vk-bridge';
 import { View, ScreenSpinner, AdaptivityProvider, AppRoot, ConfigProvider, SplitLayout, SplitCol } from '@vkontakte/vkui';
 // import '@vkontakte/vkui/dist/vkui.css';
 
-import Home from './panels/Home';
-import Persik from './panels/Persik';
 
-export default class App extends Component {
+class App extends Component {
 
 	componentDidMount(){
-		const bird = document.querySelector(".bird");
-		const gameDisplay = document.querySelector(".game-container");
-		const ground = document.querySelector(".ground-moving");
+		const canvas = document.getElementById('canvas');
+		const ctx = canvas.getContext('2d');
+		const img = new Image();
+		img.src = "https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png";
 
-		let birdLeft = 220;
-		let birdBottom = 100;
-		let gravity = 3;
-		let isGameOver = false;
-		let gap = 430;
+// general settings
+		let gamePlaying = false;
+		const gravity = .5;
+		const speed = 6.2;
+		const size = [51, 36];
+		const jump = -11.5;
+		const cTenth = (canvas.width / 10);
 
+		let index = 0,
+			bestScore = 0,
+			flight,
+			flyHeight,
+			currentScore,
+			pipe,
+			pipes;
 
-		function startGame() {
-			birdBottom -= gravity;
-			bird.style.bottom = birdBottom + "px";
-			bird.style.left = birdLeft + "px";
+// pipe settings
+		const pipeWidth = 78;
+		const pipeGap = 270;
+		const pipeLoc = () => (Math.random() * ((canvas.height - (pipeGap + pipeWidth)) - pipeWidth)) + pipeWidth;
+
+		const setup = () => {
+			currentScore = 0;
+			flight = jump;
+
+			// set initial flyHeight (middle of screen - size of the bird)
+			flyHeight = (canvas.height / 2) - (size[1] / 2);
+
+			// setup first 3 pipes
+			pipes = Array(3).fill().map((a, i) => [canvas.width + (i * (pipeGap + pipeWidth)), pipeLoc()]);
 		}
-		let gameTimerId = setInterval(startGame, 20);
 
-		function control(e) {
-			if (e.keyCode === 32) {
-				jump();
+		const render = () => {
+			// make the pipe and bird moving
+			index++;
+
+			// ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+			// background first part
+			ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index * (speed / 2)) % canvas.width) + canvas.width, 0, canvas.width, canvas.height);
+			// background second part
+			ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -(index * (speed / 2)) % canvas.width, 0, canvas.width, canvas.height);
+
+			// pipe display
+			if (gamePlaying){
+				pipes.map(pipe => {
+					// pipe moving
+					pipe[0] -= speed;
+
+					// top pipe
+					ctx.drawImage(img, 432, 588 - pipe[1], pipeWidth, pipe[1], pipe[0], 0, pipeWidth, pipe[1]);
+					// bottom pipe
+					ctx.drawImage(img, 432 + pipeWidth, 108, pipeWidth, canvas.height - pipe[1] + pipeGap, pipe[0], pipe[1] + pipeGap, pipeWidth, canvas.height - pipe[1] + pipeGap);
+
+					// give 1 point & create new pipe
+					if(pipe[0] <= -pipeWidth){
+						currentScore++;
+						// check if it's the best score
+						bestScore = Math.max(bestScore, currentScore);
+
+						// remove & create new pipe
+						pipes = [...pipes.slice(1), [pipes[pipes.length-1][0] + pipeGap + pipeWidth, pipeLoc()]];
+						console.log(pipes);
+					}
+
+					// if hit the pipe, end
+					if ([
+						pipe[0] <= cTenth + size[0],
+						pipe[0] + pipeWidth >= cTenth,
+						pipe[1] > flyHeight || pipe[1] + pipeGap < flyHeight + size[1]
+					].every(elem => elem)) {
+						gamePlaying = false;
+						setup();
+					}
+				})
 			}
-		}
-
-		function jump() {
-			if (birdBottom < 500) birdBottom += 50;
-			bird.style.bottom = birdBottom + "px";
-			console.log(birdBottom);
-		}
-		document.addEventListener("keyup", control);
-
-		function generateObstacle() {
-			let obstacleLeft = 500;
-			let randomHeight = Math.random() * 60;
-			let obstacleBottom = randomHeight;
-			const obstacle = document.createElement("div");
-			const topObstacle = document.createElement("div");
-			if (!isGameOver) {
-				obstacle.classList.add("obstacle");
-				topObstacle.classList.add("topObstacle");
+			// draw bird
+			if (gamePlaying) {
+				ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, cTenth, flyHeight, ...size);
+				flight += gravity;
+				flyHeight = Math.min(flyHeight + flight, canvas.height - size[1]);
+			} else {
+				ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, ((canvas.width / 2) - size[0] / 2), flyHeight, ...size);
+				flyHeight = (canvas.height / 2) - (size[1] / 2);
+				// text accueil
+				ctx.fillText(`Best score : ${bestScore}`, 85, 245);
+				ctx.fillText('Click to play', 90, 535);
+				ctx.font = "bold 30px courier";
 			}
-			gameDisplay.appendChild(obstacle);
-			gameDisplay.appendChild(topObstacle);
-			obstacle.style.left = obstacleLeft + "px";
-			topObstacle.style.left = obstacleLeft + "px";
-			obstacle.style.bottom = obstacleBottom + "px";
-			topObstacle.style.bottom = obstacleBottom + gap + "px";
 
-			function moveObstacle() {
-				obstacleLeft -= 2;
-				obstacle.style.left = obstacleLeft + "px";
-				topObstacle.style.left = obstacleLeft + "px";
+			document.getElementById('bestScore').innerHTML = `Best : ${bestScore}`;
+			document.getElementById('currentScore').innerHTML = `Current : ${currentScore}`;
 
-				if (obstacleLeft === -60) {
-					clearInterval(timerId);
-					gameDisplay.removeChild(obstacle);
-					gameDisplay.removeChild(topObstacle);
-				}
-				if (
-					(obstacleLeft > 200 &&
-						obstacleLeft < 280 &&
-						birdLeft === 220 &&
-						(birdBottom < obstacleBottom + 153 ||
-							birdBottom > obstacleBottom + gap - 200)) ||
-					birdBottom === 1
-				) {
-					gameOver();
-					clearInterval(timerId);
-				}
-			}
-			let timerId = setInterval(moveObstacle, 20);
-			if (!isGameOver) setTimeout(generateObstacle, 3000);
+			// tell the browser to perform anim
+			window.requestAnimationFrame(render);
 		}
-		generateObstacle();
 
-		function gameOver() {
-			clearInterval(gameTimerId);
-			console.log("game over");
-			isGameOver = true;
-			document.removeEventListener("keyup", control);
-			ground.classList.add("ground");
-			ground.classList.remove("ground-moving");
-		}
+// launch setup
+		setup();
+		img.onload = render;
+
+// start game
+		document.addEventListener('click', () => gamePlaying = true);
+		window.onclick = () => flight = jump;
+
+		console.log(gamePlaying)
 	}
-	render() {
-		return (
-			<div>
-				<div class="border-left"></div>
-				<div class="game-container">
-					<div class="border-top"></div>
-					<div class="sky">
 
-						<div class="bird"></div>
-					</div>
+render() {
+	return (
+		<div>
+			<header>
+				<h1>Floppy Bird</h1>
+				<div className="score-container">
+					<div id="bestScore"></div>
+					<div id="currentScore"></div>
 				</div>
-				<div class="ground-container">
-					<div class="ground-moving"></div>
-				</div>
-				<div class="border-right"></div>
+			</header>
 
-			</div>
-		)
-	}
+			<canvas id="canvas" width="431" height="768"></canvas>
+			<header>
+				<h1>Floppy Bird</h1>
+				<div className="score-container">
+					<div id="bestScore"></div>
+					<div id="currentScore"></div>
+				</div>
+			</header>
+
+			<canvas id="canvas" width="431" height="768"></canvas>
+		</div>
+
+	);
 }
+
+
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import bridge from "@vkontakte/vk-bridge";
 import App from "./App";
-import './index.css';
+import './css/index.css';
 
 // Init VK  Mini App
 bridge.send("VKWebAppInit");
 
-ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+ReactDOM.render(<App/>, document.getElementById("root"));
+
 if (process.env.NODE_ENV === "development") {
   import("./eruda").then(({ default: eruda }) => {}); //runtime download
 }
