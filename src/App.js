baa29c007e8156f05a1887d3235309b3e33cd564
dd114 <40179423+dd114@users.noreply.github.com>
2022-05-01 23:27:51 +0500

    game has been added

--- src/App.js
+++ src/App.js
@@ -1,52 +1,115 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import bridge from '@vkontakte/vk-bridge';
 import { View, ScreenSpinner, AdaptivityProvider, AppRoot, ConfigProvider, SplitLayout, SplitCol } from '@vkontakte/vkui';
-import '@vkontakte/vkui/dist/vkui.css';
+// import '@vkontakte/vkui/dist/vkui.css';
 
 import Home from './panels/Home';
 import Persik from './panels/Persik';
 
-const App = () => {
-	const [scheme, setScheme] = useState('bright_light')
-	const [activePanel, setActivePanel] = useState('home');
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+export default class App extends Component {
 
-	useEffect(() => {
-		bridge.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				setScheme(data.scheme)
-			}
-		});
+	componentDidMount(){
+		const bird = document.querySelector(".bird");
+		const gameDisplay = document.querySelector(".game-container");
+		const ground = document.querySelector(".ground-moving");
 
-		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
+		let birdLeft = 220;
+		let birdBottom = 100;
+		let gravity = 3;
+		let isGameOver = false;
+		let gap = 430;
+
+
+		function startGame() {
+			birdBottom -= gravity;
+			bird.style.bottom = birdBottom + "px";
+			bird.style.left = birdLeft + "px";
 		}
-		fetchData();
-	}, []);
+		let gameTimerId = setInterval(startGame, 20);
 
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
-	};
+		function control(e) {
+			if (e.keyCode === 32) {
+				jump();
+			}
+		}
 
-	return (
-		<ConfigProvider scheme={scheme}>
-			<AdaptivityProvider>
-				<AppRoot>
-					<SplitLayout popout={popout}>
-						<SplitCol>
-							<View activePanel={activePanel}>
-								<Home id='home' fetchedUser={fetchedUser} go={go} />
-								<Persik id='persik' go={go} />
-							</View>
-						</SplitCol>
-					</SplitLayout>
-				</AppRoot>
-			</AdaptivityProvider>
-		</ConfigProvider>
-	);
+		function jump() {
+			if (birdBottom < 500) birdBottom += 50;
+			bird.style.bottom = birdBottom + "px";
+			console.log(birdBottom);
+		}
+		document.addEventListener("keyup", control);
+
+		function generateObstacle() {
+			let obstacleLeft = 500;
+			let randomHeight = Math.random() * 60;
+			let obstacleBottom = randomHeight;
+			const obstacle = document.createElement("div");
+			const topObstacle = document.createElement("div");
+			if (!isGameOver) {
+				obstacle.classList.add("obstacle");
+				topObstacle.classList.add("topObstacle");
+			}
+			gameDisplay.appendChild(obstacle);
+			gameDisplay.appendChild(topObstacle);
+			obstacle.style.left = obstacleLeft + "px";
+			topObstacle.style.left = obstacleLeft + "px";
+			obstacle.style.bottom = obstacleBottom + "px";
+			topObstacle.style.bottom = obstacleBottom + gap + "px";
+
+			function moveObstacle() {
+				obstacleLeft -= 2;
+				obstacle.style.left = obstacleLeft + "px";
+				topObstacle.style.left = obstacleLeft + "px";
+
+				if (obstacleLeft === -60) {
+					clearInterval(timerId);
+					gameDisplay.removeChild(obstacle);
+					gameDisplay.removeChild(topObstacle);
+				}
+				if (
+					(obstacleLeft > 200 &&
+						obstacleLeft < 280 &&
+						birdLeft === 220 &&
+						(birdBottom < obstacleBottom + 153 ||
+							birdBottom > obstacleBottom + gap - 200)) ||
+					birdBottom === 1
+				) {
+					gameOver();
+					clearInterval(timerId);
+				}
+			}
+			let timerId = setInterval(moveObstacle, 20);
+			if (!isGameOver) setTimeout(generateObstacle, 3000);
+		}
+		generateObstacle();
+
+		function gameOver() {
+			clearInterval(gameTimerId);
+			console.log("game over");
+			isGameOver = true;
+			document.removeEventListener("keyup", control);
+			ground.classList.add("ground");
+			ground.classList.remove("ground-moving");
+		}
+	}
+	render() {
+		return (
+			<div>
+				<div class="border-left"></div>
+				<div class="game-container">
+					<div class="border-top"></div>
+					<div class="sky">
+
+						<div class="bird"></div>
+					</div>
+				</div>
+				<div class="ground-container">
+					<div class="ground-moving"></div>
+				</div>
+				<div class="border-right"></div>
+
+			</div>
+		)
+	}
 }
-
-export default App;
